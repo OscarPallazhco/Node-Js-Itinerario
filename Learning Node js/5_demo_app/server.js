178dcef30e94d7c6f1dd9ad405feabb54f607d5a
Oscar Pallazhco <oepallaz@gmail.com>
2021-09-30 01:09:06 -0500

    jquery para obtener datos de un endpoint del servidor

--- Learning Node js/5_demo_app/server.js
+++ Learning Node js/5_demo_app/server.js
@@ -4,6 +4,21 @@ var app = express();
 
 app.use(express.static(__dirname));
 
+var messages = [
+    {
+        from: 'Eduardo',
+        text: 'Hi',
+    },
+    {
+        from: 'Pedro',
+        text: 'Hi, how are you?',
+    },
+]
+
+app.get('/initialMessages', (req, res)=>{
+    res.send(messages);
+});
+
 var server = app.listen(3000, ()=>{
     console.log("Listening on port: ", server.address().port);
 });
