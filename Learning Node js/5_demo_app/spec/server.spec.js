424de72828812f661adce247d332c4861c377743
Oscar Pallazhco <oepallaz@gmail.com>
2021-10-01 01:33:34 -0500

    pruebas respecto al endpoint /messages

--- Learning Node js/5_demo_app/spec/server.spec.js
+++ Learning Node js/5_demo_app/spec/server.spec.js
@@ -1,9 +1,26 @@
+var request = require('request');
 
 // basic example
-describe('calc', ()=>{
-    it('should multiply 2 * 2 ', ()=>{
-        expect(2*2).toBe(4)
+// describe('calc', ()=>{
+//     it('should multiply 2 * 2 ', ()=>{
+//         expect(2*2).toBe(4)
+//     });
+// });
+
+describe('get messages', ()=>{
+    
+    it('should get 200 status code', (done)=>{
+        request.get('http://localhost:3000/messages', (err, res)=>{
+            expect(res.statusCode).toEqual(200);
+            done();
+        });
     });
+    
+    it('should return a not empty list', (done)=>{
+        request.get('http://localhost:3000/messages', (err, res)=>{
+            expect(JSON.parse(res.body).length).toBeGreaterThan(0);
+            done();
+        });
+    });
+
 });
-
-
