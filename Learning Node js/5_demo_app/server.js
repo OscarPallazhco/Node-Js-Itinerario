6a89f56dae600ad901a70f254c8c866e81114a2c
Oscar Pallazhco <oepallaz@gmail.com>
2021-10-01 00:34:05 -0500

    async await como solución al problema de excesivos callbacks

--- Learning Node js/5_demo_app/server.js
+++ Learning Node js/5_demo_app/server.js
@@ -23,15 +23,21 @@ app.get('/messages', (req, res)=>{
     });
 });
 
-app.post('/messages', (req, res)=>{
-    var message = new Message(req.body);
-    message.save((err)=>{
-        if (err) {
-            res.sendStatus(500);
+app.post('/messages', async (req, res)=>{
+    try {
+        var message = new Message(req.body);
+        await message.save();
+        var censored = await Message.findOne({text: 'badword'})
+        if (censored) {
+            await Message.deleteOne({_id: censored.id});
+        }else{
+            io.emit('message', req.body);
         }
-        io.emit('message', req.body);
         res.sendStatus(200);
-    });
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
 });
 
 io.on('connection', (socket)=>{
