ce403964b965f5787b5b86f168826051791afb09
Oscar Pallazhco <oepallaz@gmail.com>
2021-10-16 01:34:39 -0500

    ejs template engine

--- 3. Building a Website with Node.js and Express.js/README.md
+++ 3. Building a Website with Node.js and Express.js/README.md
@@ -1,1 +1,6 @@
 https://www.linkedin.com/learning/building-a-website-with-node-js-and-express-js-3/dynamic-websites-with-node-and-express
+
+
+EJS  
+Motor para el uso de templates, es necesario instalar una extensión para que vsc reconozca los archivos ejs.  
+VS Marketplace Link: https://marketplace.visualstudio.com/items?itemName=DigitalBrainstem.javascript-ejs-support  
--- 3. Building a Website with Node.js and Express.js/web_app/server.js
+++ 3. Building a Website with Node.js and Express.js/web_app/server.js
@@ -3,12 +3,20 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+const STATIC_PATH = path.join(__dirname, 'static');
+const VIEWS_PATH = path.join(__dirname, 'views');
+const INDEX_PATH = path.join(__dirname, 'static', 'index.html');
+
+app.set('view engine', 'ejs');
+app.set('views', VIEWS_PATH);
+
 // usar el middleware static de express, toda petición al servidor 
 // pasará por este middleware
-app.use(express.static(path.join(__dirname, 'static')))
+app.use(express.static(STATIC_PATH));
 
 app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'static', 'index.html'))
+    // buscará el archivo pages/index dentro de la carpeta que se definió para views
+    res.render('pages/index', {pageTitle: 'webPage with Express.js'})
 });
 
 
