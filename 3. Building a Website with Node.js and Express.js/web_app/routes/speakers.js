9a23de0116a862a1a89aa1da601f089c46df40e2
Oscar Pallazhco <oepallaz@gmail.com>
2021-10-19 00:56:06 -0500

    list of speakers

--- 3. Building a Website with Node.js and Express.js/web_app/routes/speakers.js
+++ 3. Building a Website with Node.js and Express.js/web_app/routes/speakers.js
@@ -6,16 +6,21 @@ const path = require('path');
 const SpeakersService = require('./../services/SpeakerService');
 const speakersService = new SpeakersService(path.join(__dirname, '..', 'data', 'speakers.json'));
 
-router.get('/', async (req, res)=>{
-    // ejemplo del uso de cookies para contar la cantidad de viitas a la pag
-    if (!req.session.speakersVisitedCount) {
-        req.session.speakersVisitedCount = 0;
-    }
-    req.session.speakersVisitedCount++;    
-    // console.log(req.session.speakersVisitedCount);
+// router.get('/', async (req, res)=>{
+//     // ejemplo del uso de cookies para contar la cantidad de viitas a la pag
+//     if (!req.session.speakersVisitedCount) {
+//         req.session.speakersVisitedCount = 0;
+//     }
+//     req.session.speakersVisitedCount++;    
+//     // console.log(req.session.speakersVisitedCount);
 
+//     const speakers = await speakersService.getList();
+//     return res.json(speakers);
+// });
+
+router.get('/', async (req, res)=>{
     const speakers = await speakersService.getList();
-    return res.json(speakers);
+    res.render('layout', {pageTitle: 'Speakers', template: 'speakers', speakers});
 });
 
 router.get('/:user', (req, res)=>{
