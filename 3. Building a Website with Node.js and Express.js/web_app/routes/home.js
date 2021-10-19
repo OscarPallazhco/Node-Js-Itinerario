660c88882c7902e5f5c9668d48821f8953ddff45
Oscar Pallazhco <oepallaz@gmail.com>
2021-10-19 00:33:15 -0500

    loop in templates

--- 3. Building a Website with Node.js and Express.js/web_app/routes/home.js
+++ 3. Building a Website with Node.js and Express.js/web_app/routes/home.js
@@ -1,9 +1,15 @@
 const {Router} = require('express');
 const router = Router();
+const path = require('path');
 
-router.get('/', (req, res)=>{
+// services
+const SpeakersService = require('./../services/SpeakerService');
+const speakersService = new SpeakersService(path.join(__dirname, '..', 'data', 'speakers.json'));
+
+router.get('/', async (req, res)=>{
     // buscará el archivo pages/index dentro de la carpeta que se definió para views
-    res.render('layout', {pageTitle: 'webPage with Express.js', template: 'index'});
+    const topSpeakers = await speakersService.getList();
+    res.render('layout', {pageTitle: 'webPage with Express.js', template: 'index', topSpeakers});
 });
 
 module.exports = router;
