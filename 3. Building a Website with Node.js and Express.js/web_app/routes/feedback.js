85e4177ed4f3e45367e7677fe5de378fc75e7c86
Oscar Pallazhco <oepallaz@gmail.com>
2021-10-20 17:41:58 -0500

    save feedback in the file

--- 3. Building a Website with Node.js and Express.js/web_app/routes/feedback.js
+++ 3. Building a Website with Node.js and Express.js/web_app/routes/feedback.js
@@ -11,8 +11,9 @@ router.get('/', async (req, res, next)=>{
     try {
         const feedback = await feedbackService.getList();
         const errors = req.session.feedback ? req.session.feedback.errors : false;
+        const success = req.session.feedback ? req.session.feedback.success : false;
         req.session.feedback = {};
-        return res.render('layout', {pageTitle: 'Feedback', template: 'feedback', feedback, errors});
+        return res.render('layout', {pageTitle: 'Feedback', template: 'feedback', feedback, errors, success});
     } catch (error) {
         return next(error);
     }
@@ -24,7 +25,7 @@ router.post('/',[
     check('email').trim().isEmail().normalizeEmail().withMessage('A valid email is required'),
     check('title').trim().isLength({min:3}).escape().withMessage('A title is required'),
     check('message').trim().isLength({min:5}).escape().withMessage('A name is required'),
-], (req, res, next)=>{
+], async (req, res, next)=>{
     try {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -33,7 +34,12 @@ router.post('/',[
             };
             return res.redirect('/feedback')
         }
-        res.send("feedback posted");
+        const {name, email, title, message} = req.body;
+        await feedbackService.addEntry(name, email, title, message);
+        req.session.feedback = {
+            success: 'thanks for you feedback'
+        };
+        return res.redirect('/feedback')
     } catch (error) {
         return next(error);
     }
