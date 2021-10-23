6c97cb239791d0a0710de01848ea8d52ec98a835
Oscar Pallazhco <oepallaz@gmail.com>
2021-10-22 23:55:54 -0500

    connect to mongodb

--- 4. Databases for Node.js Developers/linkedin-node-databases/maxcoin/services/backend/MongoBackend.js
+++ 4. Databases for Node.js Developers/linkedin-node-databases/maxcoin/services/backend/MongoBackend.js
@@ -1,15 +1,28 @@
 /* eslint-disable no-useless-constructor */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-empty-function */
+const { MongoClient } = require('mongodb');
 const CoinAPI = require('../CoinAPI');
+require('dotenv').config();
 
 class MongoBackend {
 
   constructor() {
     this.coinAPI = new CoinAPI();
+    this.mongoUrl = process.env.DB_CNN;
+    this.client = null;
+    this.collection = null;
   }
 
-  async connect() {}
+  async connect() {
+    const mongoClient = new MongoClient(this.mongoUrl, {
+      useUnifiedTopology: true,
+      useNewUrlParser : true,
+    });
+    this.client = await mongoClient.connect();
+    this.collection = this.client.db('maxcoin').collection('values');
+    return this.client;
+  }
 
   async disconnect() {}
 
@@ -17,7 +30,17 @@ class MongoBackend {
 
   async getMax() {}
 
-  async max() {}
+  async max() {
+    console.info('Connection to MongoDB');
+    console.time('mongodb-connect');
+    const client = await this.connect();
+    if (client.isConnected()) {
+      console.info('Connected to MongoDB');
+    }else{
+      console.info('Fail to connected to MongoDB');
+    }
+    console.timeEnd('mongodb-connect');
+  }
 }
 
 module.exports = MongoBackend;
